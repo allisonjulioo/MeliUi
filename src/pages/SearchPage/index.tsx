a7c96ac36d138a27feedd40b8a2f748f69c9ec19
Allison Nunes <allison.nunes@maxmilhas.com.br>
2022-12-28 05:07:34 -0300

    Add label to initial page

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  min-height: 50vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const SearchPage = () => {
-  return <div id='search-page' />;
+  return (
+    <Container id='search-page'>
+      <p>Pesquise algo na caixa de pesquisa</p>
+    </Container>
+  );
 };
 
 export {SearchPage};
